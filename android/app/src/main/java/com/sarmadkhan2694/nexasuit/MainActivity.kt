7070773b7758c0250e069b76d244c5af96ddb92b
unknown <msarmad2694@gmail.com>
2025-09-19 12:29:17 +0500

    changes

--- android/app/src/main/java/com/sarmadkhan2694/nexasuit/MainActivity.kt
+++ android/app/src/main/java/com/sarmadkhan2694/nexasuit/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sarmad_khan2694.nexasuit
+package com.sarmadkhan2694.nexasuit
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/sarmadkhan2694/nexasuit/MainApplication.kt
+++ android/app/src/main/java/com/sarmadkhan2694/nexasuit/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.sarmad_khan2694.nexasuit
+package com.sarmadkhan2694.nexasuit
 
 import android.app.Application
 import android.content.res.Configuration
